94e572b16372ef69a2854ecf6a9efd08465c53ef
Alain Vagner <alain.vagner@sip.etat.lu>
2022-04-11 16:59:00 +0200

    initial release

---
+++ main.js
@@ -0,0 +1,158 @@
+import { getSyncedDatasets, createDataset, deleteDataset, getDataset, genTags, genResources, updateDataset, genDescription} from './odp.js'
+import { eqSet, eqResources, fetchThrottle } from './utils.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const changesEnabled = true
+
+// the dotstat platform has 2 interesting endpoints regarding metadata: /config and /search
+// the 2 following functions enable to get the data from these 2 endpoints
+async function getConfig() {
+    try {
+        const res = await fetchThrottle(process.env.lustatURL+"/config", {
+        "credentials": "omit",
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        "body": "{\"lang\":\"fr\",\"facets\":{\"datasourceId\":[\"release\"]}}",
+        "method": "POST",
+        "mode": "cors"
+        })
+        if (!res.ok) {
+            res.text().then(t => { throw t})
+        }
+        return res.json()
+    } catch(e) {
+        console.error(e)
+        return {}
+    }
+}
+
+async function getData(topic) {
+    try {
+        const response = await fetchThrottle(process.env.lustatURL+"/search", {
+            "credentials": "omit",
+            "headers": {
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            "body": "{\"lang\":\"fr\",\"search\":\"\",\"facets\":{\"Thèmes\":[\""+topic+"\"], \"datasourceId\":[\"release\"]},\"rows\":10000,\"start\":0}",
+            "method": "POST"
+        })
+        if (!response.ok) {
+            response.text().then(t => { throw t})
+        }
+        return response.json()
+    } catch(e) {
+        console.error(e)
+        return {}
+    }
+}
+
+// convert a topic ID to an user readable topic name
+function getTopicLabel(topic) {
+    return topic.replace(/^\d+\|/, '').replace(/#.+?#/g,'').replace(/\|/g, ' - ')
+}
+
+// extract keywords from resources to include them on the dataset level 
+function getKeywordsFromResources(resources) {
+    const keywords = resources.map(f => {return f.description.split("Mots-clés:")[1]}).filter(f=> {return f!=undefined}).flatMap(f=> {return f.split(',')}).map(f=> {return f.trim().toLowerCase()}).map(f=>{return f.replace(/[\s\']/g, '-')})
+    return [...new Set(keywords)]
+}
+
+getSyncedDatasets().then(d => {
+    let odpMapping = {}
+    d.data.map(e => {return [e.id, e.extras['harvest:remote_id']]}).forEach(tuple => { odpMapping[tuple[1]] = tuple[0] } )
+    const odpIds = new Set(d.data.map(e => {return  e.extras['harvest:remote_id']}))
+
+    getConfig().then(data => {
+        // FIXME: here we keep only topics of level 2, should be discussed, currently different
+        const topicsSet = new Set(data['facets']['Thèmes']['buckets'].filter(e => { return (e.val.split('|').length -1 == 2)}).map(e => {return e.val}))
+        const topicsArr = [...topicsSet]
+
+        // get the list of items that were added, deleted, changed
+        const toDelete = new Set([...odpIds].filter(x => !topicsSet.has(x)))
+        const toAdd = new Set(topicsArr.filter(x => !odpIds.has(x)))
+        const rest = new Set([...odpIds].filter(x => topicsSet.has(x)))
+
+        console.log('to be deleted', [...toDelete].length)
+        console.log('to be added', [...toAdd].length)
+        console.log('check for updates', [...rest].length)
+
+        // delete what needs to be deleted
+        if (changesEnabled) {
+            toDelete.forEach(e => {
+               deleteDataset(odpMapping[e]).then(a => { console.log('Dataset deletion', (a)?'succeeded': 'failed', 'for', e)})
+            })
+        }
+
+        Promise.all(topicsArr.map(t => {return getData(t)})).then(r => { 
+            const dataflows = r.map(d => d.dataflows)
+
+            // get resources for each topic
+            let resources = {}
+            for (let i=0; i<topicsArr.length; i++) {
+                resources[topicsArr[i]] = dataflows[i]
+            }
+
+            // add what needs to be added
+
+            if (changesEnabled) {
+                toAdd.forEach(id => {
+                    let keywords = getKeywordsFromResources(resources[id])
+                    const label = getTopicLabel(id)
+                    createDataset(label, resources[id], id, keywords).then(e => {console.log('Dataset successfully added for', id)}).catch(e=> console.error(e))
+                })
+            }
+            // check if the rest should be modified
+            rest.forEach(id => {
+                // get matching dataset from ODP
+                console.log('Checking updates for', id)
+                getDataset(odpMapping[id]).then(dataset => {
+                    // compare keywords
+
+                    // WARNING: tags should always be present in an update request, otherwise they are wiped out (bug in udata?)
+                    const statecName = getTopicLabel(id)
+                    const odpName = dataset.title
+                    const statecTags = new Set(genTags(statecName, getKeywordsFromResources(resources[id])))
+                    const odpTags = new Set(dataset.tags)
+
+                    if (!eqSet(statecTags, odpTags)) {
+                        console.log('Tags should be updated for', dataset.id)
+                        //console.log('old:', [...odpTags], 'new:', [...statecTags])
+                        if (changesEnabled){
+                            updateDataset(dataset.id, {'tags': [...statecTags]}).then(f => {console.log('Tags successfully updated for', dataset.id)}).catch(e=>{console.error(e)})
+                        }
+                    }
+
+                    // compare name
+                    if (statecName != odpName) {
+                        console.log('Title should be updated for', dataset.id)
+                        //console.log('old:', odpName, 'new:', statecName )
+                        if (changesEnabled) {
+                            updateDataset(dataset.id, {'title': statecName, 'tags': [...statecTags]}).then(f => {console.log('Title successfully updated for:', dataset.id)}).catch(e=>{console.error(e)})
+                        }
+                    } 
+
+                    // compare resources
+                    const statecResources = new Set(genResources(resources[id]).map(e => {return {"title": e.title, "description": e.description, "url": e.url}}))
+                    const odpResources = new Set(dataset.resources.map(e => {return {"title": e.title, "description": e.description, "url": e.url}}))
+                    if (!eqResources(statecResources, odpResources)) {
+                        console.log('Resources and decription should be updated for', dataset.id)
+                        //console.log('old:', odpResources, 'new:', statecResources)
+                        // update resources + desc
+                        if (changesEnabled) {
+                            genDescription(genResources(resources[id])).then(desc => {
+                                updateDataset(dataset.id, {'resources': genResources(resources[id]), 'description': desc, 'tags': [...statecTags] }).then(f => {console.log('Resources and description successfully updated for', dataset.id)}).catch(e=>{console.error(e)})
+                            })
+                        }
+
+                    } 
+                }).catch(e => console.error(e))
+            })
+        }).catch(e => { console.error(e) })
+    }).catch(e => {console.error(e)})
+}).catch(e => {console.error(e)})
+
---
+++ odp.js
@@ -0,0 +1,184 @@
+import ejs from 'ejs'
+import keyword_extractor from 'keyword-extractor'
+import dotenv from 'dotenv'
+import { fetchThrottle } from './utils.js'
+
+dotenv.config()
+
+const odpURL = process.env.odpURL
+const odpAPIKey = process.env.odpAPIKey
+const statecOrgId = process.env.statecOrgId
+const statecTag = 'statec-sync'
+
+async function getSyncedDatasets() {
+    try {
+        // FIXME: manage pagination, temporarily a large page size here
+        const res = await fetchThrottle(odpURL+"/datasets/?tag="+statecTag+"&page=0&page_size=200", {
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            'X-API-KEY': odpAPIKey
+        },
+        "method": "GET"
+        })
+        if (!res.ok) {
+            res.text().then(t => { throw t})
+        }
+        return res.json()
+    } catch(e) {
+        console.error(e)
+        return {}
+    }    
+}
+
+function genTags(title, keywords) {
+    let tags = keyword_extractor.extract(title, {
+        language: 'french', 
+        remove_digits: false,
+        return_changed_case: true, 
+        remove_duplicates: true
+    })
+    tags = tags.concat(keywords)
+    tags = tags.map(t => { return t.normalize("NFD").replace(/[\u0300-\u036f]/g, "") }) // remove accents
+    tags = tags.map(t => {return t.replace(/\./g, '')}) // remove special characters (dots for now) because udata removes them and we need to be able to compare
+    tags = [... new Set(tags)] // remove duplicates
+    tags = tags.filter(t => {return (t.length >= 3)})
+    tags = tags.concat([ statecTag, 'statistics', 'statistiques'])
+    return tags
+}
+
+function genResources(resources) {
+    return resources.map(r => {return {
+        "description": r.description,
+        "filesize": 0,
+        "filetype": "remote",
+        "format": "html",
+        "title": r.name,
+        "type": "other",
+        "url": "https://lustat.statec.lu/vis?df%5Bds%5D=release&df%5Bid%5D="+r.dataflowId+"&df%5Bag%5D=LU1"
+      }
+    })
+}
+
+async function genDescription(resources) {
+    const p = new Promise((resolve,reject) => {
+        ejs.renderFile('./desc.ejs', {resources: resources.map(r => {return r.title}) }, function(err, desc){
+            if (err !== null) {
+                reject(err)
+            }
+            resolve(desc)
+        })
+    })
+    return p.then(d => {return d})    
+}
+
+async function createDataset(title, resources, remote_id, keywords =[]) {
+    const tags = genTags(title, keywords)
+    resources = genResources(resources)
+
+    return genDescription(resources).then(desc => {
+        const dataset = {
+            "description": desc, 
+            "frequency": "unknown", 
+            "license": 'cc-zero', 
+            "organization": {"id": statecOrgId }, 
+            "tags": tags,
+            "title": title,
+            "extras": { "harvest:domain" : "lustat.statec.lu", "harvest:remote_id": remote_id }
+        }
+        if (resources.length != 0) {
+            dataset.resources = resources
+        }
+        return createDatasetFromJSON(dataset)
+    })
+}
+
+async function createDatasetFromJSON(dataset) {
+    if (dataset != null) {
+        try {
+            const res = await fetchThrottle(odpURL+"/datasets/", {
+            "headers": {
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json;charset=utf-8",
+                'X-API-KEY': odpAPIKey
+            },
+            "body": JSON.stringify(dataset),
+            "method": "POST"
+            })
+
+            if (!res.ok) {
+                res.text().then(t => { throw t})
+            }
+            return res.json()
+        } catch(e) {
+            console.error(e)
+            return {}
+        }
+    } else {
+        console.error('cannot create empty dataset')
+    }
+}
+
+async function deleteDataset(id) {
+    try {
+        const res = await fetchThrottle(odpURL+"/datasets/"+id+"/", {
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json;charset=utf-8",
+            'X-API-KEY': odpAPIKey
+        },
+        "method": "DELETE"
+        })
+        
+        return (res.ok)
+    } catch(e) {
+        console.error(e)
+        return false
+    }    
+}
+
+
+async function getDataset(id) {
+    try {
+        const res = await fetchThrottle(odpURL+"/datasets/"+id+"/", {
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json;charset=utf-8",
+            'X-API-KEY': odpAPIKey
+        },
+        "method": "GET"
+        })
+        if (!res.ok) {
+            res.text().then(t => { throw t})
+        }
+
+        return res.json()
+    } catch(e) {
+        console.error(e)
+        return {}
+    }    
+}
+
+async function updateDataset(id, payload) {
+    try {
+        const res = await fetchThrottle(odpURL+"/datasets/"+id+"/", {
+        "headers": {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json;charset=utf-8",
+            'X-API-KEY': odpAPIKey
+        },
+        "body": JSON.stringify(payload),
+        "method": "PUT"
+        })
+        if (!res.ok) {
+            res.text().then(t => { throw t})
+        }
+        return res.json()
+    } catch(e) {
+        console.error(e)
+        return {}
+    }    
+}
+
+export { getSyncedDatasets, getDataset, createDataset, deleteDataset, genTags, genResources, updateDataset, genDescription }
+
+
